2eb06e417072a73af6ec68fd9a24072ed0d85316
Romana Mahjabin Eshita <nullcharacter97@gmail.com>
2020-12-08 01:57:39 +0600

    complete initledger

--- chaincode/fabcar/go/fabcar.go
+++ chaincode/fabcar/go/fabcar.go
@@ -53,7 +53,7 @@ type MetaDataStore struct {
 	Doctype  string //`json:"docType"`
 	User     string //`json:"user"`
 	Metadata string //`json:"metaData"`
-	//Key string
+	Key string
 }
 type List struct {
 	Tal string
@@ -148,15 +148,16 @@ func (s *SmartContract) initLedger(APIstub shim.ChaincodeStubInterface) sc.Respo
 	// 	fmt.Println("Added", cars[i])
 	// 	i = i + 1
 	// }
-	metaDatas := []MetaDataStore{
-		{Doctype: "MetaData Store", User: "www.idp1.org", Metadata: "entityid: \"https://mail.service.com/service/extension/samlreceiver \",\n  contacts: [],\n  \"metadata-set\": \"saml20-sp-remote\",\n  AssertionConsumerService: [\n    {\n      Binding: \"urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST\",\n      Location: \"https://mail.service.com/service/extension/samlreceiver\",\n      index: 0,\n    },\n  ],\n  SingleLogoutService: [],\n  \"validate.authnrequest\": false,\n  \"NameIDFormat\": \"urn:oasis:names:tc:"},
+	metaDatas := MetaDataStore{
+		Doctype: "MetaData Store", User: "www.idp1.org", Metadata: "entityid: \"https://mail.service.com/service/extension/samlreceiver \",\n  contacts: [],\n  \"metadata-set\": \"saml20-sp-remote\",\n  AssertionConsumerService: [\n    {\n      Binding: \"urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST\",\n      Location: \"https://mail.service.com/service/extension/samlreceiver\",\n      index: 0,\n    },\n  ],\n  SingleLogoutService: [],\n  \"validate.authnrequest\": false,\n  \"NameIDFormat\": \"urn:oasis:names:tc:", Key: "000"
 	}
+	
 	// for i, metaData := range metaDatas {
 	// 	metaDataBytes, _ := json.Marshal(metaData)
 	// 	 APIstub.PutState("MetaData"+strconv.Itoa(i), metaDataBytes)
 	// }
 	metaDataBytes, _ := json.Marshal(metaDatas)
-	APIstub.PutState("MetaData"+strconv.Itoa(1), metaDataBytes)
+	APIstub.PutState(metaDatas.Key, metaDataBytes)
 	talList := TalList{
 		Doctype:  "TAL List",
 		EntityId: "www.idp.sust.com",
@@ -180,7 +181,7 @@ func (s *SmartContract) initLedger(APIstub shim.ChaincodeStubInterface) sc.Respo
 		Key: "0002",
 	}
 	talListBytes1, _ := json.Marshal(talListSp1)
-	APIstub.PutState("MetaData"+strconv.Itoa(1), talListBytes1)
+	APIstub.PutState(talListSp1.Key, talListBytes1)
 
 	talListSp2 := TalList{
 		Doctype:  "TAL List",
@@ -191,8 +192,6 @@ func (s *SmartContract) initLedger(APIstub shim.ChaincodeStubInterface) sc.Respo
 		Key: "0003",
 	}
 	talListBytes2, _ := json.Marshal(talListSp2)
-	APIstub.PutState(talListSp2.Key, talListBytes2)
-	talListBytes2, _ = json.Marshal(talListSp2)
 	APIstub.PutState(talListSp2.Key, talListBytes2)
 
 	return shim.Success(nil)
