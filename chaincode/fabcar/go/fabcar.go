82d5c3c218f7a11650f8cf543f9ccd70daed99f3
Romana Mahjabin Eshita <nullcharacter97@gmail.com>
2020-12-11 12:25:21 +0600

    user detch added

--- chaincode/fabcar/go/fabcar.go
+++ chaincode/fabcar/go/fabcar.go
@@ -228,7 +228,7 @@ func (s *SmartContract) userFetch(APIstub shim.ChaincodeStubInterface, args []st
 		queryResponse, _ := resultsIterator.Next()
 		_ = json.Unmarshal(queryResponse.Value, codeData)
 	}
-	return shim.response(codeData.User)
+	return shim.Success(codeData.User)
 }
 func (s *SmartContract) queryAllCars(APIstub shim.ChaincodeStubInterface) sc.Response {
 
