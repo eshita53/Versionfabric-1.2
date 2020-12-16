d03101b66afb2575d3652bcb36e64cceaca479ce
Romana Mahjabin Eshita <nullcharacter97@gmail.com>
2020-12-17 00:08:15 +0600

    query metadata added

--- chaincode/fabcar/go/fabcar.go
+++ chaincode/fabcar/go/fabcar.go
@@ -322,19 +322,15 @@ func (s *SmartContract) userFetch(APIstub shim.ChaincodeStubInterface, args []st
 	return codeData.User
 }
 func (s *SmartContract) fetch(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+	//user := strings.ToLower(args[0])
 	user := args[0]
 	queryString := fmt.Sprintf("{\"selector\": {\"Doctype\": \"MetaData Store\",\"User\": \"%s\"}}", user)
-	resultsIterator, _ := APIstub.GetQueryResult(queryString)
-	defer resultsIterator.Close()
-	var results []QueryResultMetaData
-	codeData := new(MetaDataStore)
-	for resultsIterator.HasNext() {
-		queryResponse, _ := resultsIterator.Next()
-		_ = json.Unmarshal(queryResponse.Value, codeData)
-		queryResult := QueryResultMetaData{Key: queryResponse.Key, Record: codeData}
-		results = append(results, queryResult)
-	}
-	return shim.Success(results)
+	queryResults, _ := getQueryResultForQueryString(APIstub, queryString)
+	// if  err != nil {
+	// 	return shim.Error(err.error())
+	// }
+
+	return shim.Success(queryResults)
 }
 
 func (s *SmartContract) queryAllCars(APIstub shim.ChaincodeStubInterface) sc.Response {
@@ -396,6 +392,50 @@ func (s *SmartContract) changeCarOwner(APIstub shim.ChaincodeStubInterface, args
 	APIstub.PutState(args[0], carAsBytes)
 
 	return shim.Success(nil)
+}
+
+///get query result by query string
+
+func getQueryResultForQueryString(stub shim.ChaincodeStubInterface, queryString string) ([]byte, error) {
+
+	fmt.Printf("- getQueryResultForQueryString queryString:\n%s\n", queryString)
+
+	resultsIterator, err := stub.GetQueryResult(queryString)
+	if err != nil {
+		return nil, err
+	}
+	defer resultsIterator.Close()
+
+	// buffer is a JSON array containing QueryRecords
+	var buffer bytes.Buffer
+	buffer.WriteString("[")
+
+	bArrayMemberAlreadyWritten := false
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return nil, err
+		}
+		// Add a comma before array members, suppress it for the first array member
+		if bArrayMemberAlreadyWritten == true {
+			buffer.WriteString(",")
+		}
+		buffer.WriteString("{\"Key\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(queryResponse.Key)
+		buffer.WriteString("\"")
+
+		buffer.WriteString(", \"Record\":")
+		// Record is a JSON object, so we write as-is
+		buffer.WriteString(string(queryResponse.Value))
+		buffer.WriteString("}")
+		bArrayMemberAlreadyWritten = true
+	}
+	buffer.WriteString("]")
+
+	fmt.Printf("- getQueryResultForQueryString queryResult:\n%s\n", buffer.String())
+
+	return buffer.Bytes(), nil
 }
 
 // The main function is only relevant in unit test mode. Only included here for completeness.
---
+++ chaincode/fabcar/go/query.go
@@ -0,0 +1,11 @@
+package main
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	sc "github.com/hyperledger/fabric/protos/peer"
+)
+
+
