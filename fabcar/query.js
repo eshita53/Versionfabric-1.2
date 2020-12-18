239397495130b6daaff00f3e816263a9976c3b3b
Romana Mahjabin Eshita <nullcharacter97@gmail.com>
2020-12-18 10:21:49 +0600

    talList delete added

--- fabcar/query.js
+++ fabcar/query.js
@@ -54,8 +54,8 @@ Fabric_Client.newDefaultKeyValueStore({ path: store_path
 	const request = {
 		//targets : --- letting this default to the peers assigned to the channel
 		chaincodeId: 'fabcar',
-		fcn: 'approval',
-		args: ['www.sp2.sust.com']
+		fcn: 'removeApproval',
+		args: ['www.sp2.sust.com','www.idp.sust.com']
 	};
 
 	// send the query proposal to the peer
